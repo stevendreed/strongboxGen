b6380c9eeed70c5ce9a7d69bcf011f3b0b59a673
Steven Reed <steven.daniel.reed@gmail.com>
2023-08-20 12:52:49 -0700

    added prototypes for myRand and mySeeder

---
+++ Develop/rand.js
@@ -0,0 +1,21 @@
+/* 
+rand.js
+
+this file prototypes {1} myRand() and {2} mySeeder functions.
+
+these functions, when used in conjunction, randomly generate ASCII range values
+which can be used to generate the actual random component of password elements
++=============================================================================+
+*/
+
+function myRand(seed)
+{
+    return 
+}
+
+function mySeeder()
+{
+    // filler function that returns an integer based on epoch time
+    return Math.floor(Date() / 1024);
+}
+
