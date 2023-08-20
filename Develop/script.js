b917062e7e3cfcff7d094f74bdc65f391cbcf432
Steven Reed <steven.daniel.reed@gmail.com>
2023-08-20 12:30:53 -0700

    added usrInput function w/ default arg

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,22 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword()
+{
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+}
+function usrInput(MAXLEN)
+{
+  // adds default behavior to usrInput() function
+  if (typeof MAXLEN === 'undefined')
+  {
+    MAXLEN = 64;
+  }
+  
 }
 
 // Add event listener to generate button
