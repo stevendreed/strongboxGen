9c8f575acff17e26cb1f52475b8887ba4d2b3012
stevendreed <steven.daniel.reed@gmail.com>
2023-08-25 15:10:08 -0700

    removed unneeded function

--- Develop/script.js
+++ Develop/script.js
@@ -6,26 +6,33 @@ functionality to index.html, and build out elements in response to these
 inputs
 +=============================================================================+
 */
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector(`#generate`);
 
 // Write password to the #password input
 function writePassword()
 {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector(`#password`);
 
   passwordText.value = password;
+  // build a list of elements for: lower/uppercase, numeric, special chars,
+  // and length of password (7 < n < 129)
+
+
+
+
+  // render password onto the page
 
 }
 
-function usrInput(MAXLEN)
-{
-  // adds default behavior to usrInput() function
-  if (typeof MAXLEN === 'undefined')
-  {
-    MAXLEN = 64;
-  }
-}
+// function usrInput(MAXLEN)
+// {
+//   // adds default behavior to usrInput() function
+//   if (typeof MAXLEN === `undefined`)
+//   {
+//     MAXLEN = 64;
+//   }
+// }
 
 // [# of caps, # of nums, # symbols, # total]
 function pwGen(inputArgs)
@@ -45,4 +52,4 @@ function pwGen(inputArgs)
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener(`click`, writePassword());
